a8b687f5e51a432a631774b1feea8c5bc8ee99cc
Milinserv <107671454+Milinserv@users.noreply.github.com>
2024-04-01 16:43:27 +0400

    deleted button in layout

--- resources/js/layouts/Layout.vue
+++ resources/js/layouts/Layout.vue
@@ -56,12 +56,6 @@ const dropdown = () => {
             </div>
         </aside>
         <div class="pl-72 h-20 border border-bottom content-center">
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <button type="button" class="text-gray-900 bg-white border border-gray-300
-                 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium
-                 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white
-                 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Дома под ключ</button>
-            </div>
         </div>
         <article>
             <slot />
